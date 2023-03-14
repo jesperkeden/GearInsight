a877d5c303a3ccc2ad8df4e2f81ab9fa73c653ba
andreastollmar <andreastollmar@gmail.com>
2023-03-14 13:56:41 +0100

    loading

--- Views/LoadingPage.xaml.cs
+++ Views/LoadingPage.xaml.cs
@@ -11,11 +11,18 @@ public partial class LoadingPage : ContentPage
     {
         base.OnAppearing();
 
-        Task load = LoadingBar.ProgressTo(1, 8000, Easing.Linear);
+        //Task load = LoadingBar.ProgressTo(1, 8000, Easing.Linear);
         
-        await load;
-        await Navigation.PopAsync();
-        await Shell.Current.GoToAsync(nameof(CharacterPage));
+        //await load;
+        //await Navigation.PopAsync();
+        //await Shell.Current.GoToAsync(nameof(CharacterPage));
 
+    }
+
+    private async void TestButton_Clicked(object sender, EventArgs e)
+    {
+        Task test = LoadingBar.ProgressTo(1, 2000, Easing.Linear);
+        await test;
+        LoadingBar.Progress = 0;
     }
 }
